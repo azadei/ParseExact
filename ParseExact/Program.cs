04322708e1fdb9c2c2b5676a49de4686563d18d7
azadei <azadei@code.edu.az>
2023-07-06 15:22:58 +0400

    1

---
+++ ParseExact/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ParseExact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                var date = DateTime.ParseExact("16_06_1992", "dd_MM_yyyy", CultureInfo.InvariantCulture);
+                Console.WriteLine("Date:" + date.ToString());
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Invalid date format");
+            }
+        }
+    }
+}
